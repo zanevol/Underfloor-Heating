50992216ab2d7c6ed8e94aa978696960782b714a
zanevoll <zanevoll@gmail.com>
2023-06-15 20:40:49 +0700

    feat: calculator
    - add calculator logic

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ new Swiper('.hero__slider', {
     spaceBetween: 10,
     loop: true,
     navigation: {
-      nextEl: '.hero__slider-btn_next',
-      prevEl: '.hero__slider-btn_prev'
+        nextEl: '.hero__slider-btn_next',
+        prevEl: '.hero__slider-btn_prev'
     },
     autoplay: {
         delay: 3000
@@ -32,3 +32,23 @@ new Swiper('.hero__slider', {
 const footerCopyrightYear = document.querySelector('.footer__copyright span');
 const currentYear = new Intl.DateTimeFormat('RU-ru', {year: 'numeric'}).format(new Date());
 footerCopyrightYear.textContent = currentYear;
+
+//Calculator
+const calcForm = document.querySelector('.calc__form');
+const calcResultSquare = document.querySelector('.calc__result-square');
+const calcResultLength = document.querySelector('.calc__result-length');
+const tariff = {
+    economy: 550,
+    comfort: 1400,
+    premium: 2700
+};
+calcForm.addEventListener('submit', event => {
+    event.preventDefault();
+    if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+        const square = calcForm.width.value * calcForm.length.value;
+        const price =  square * tariff[calcForm.rate.value];
+        calcResultSquare.textContent = String(square);
+        calcResultLength.textContent = String(price);
+        calcForm.reset();
+    }
+})
